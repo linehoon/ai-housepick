a90f08564e3232252f69efb2f5078efe8b31bbaf
linehoon <83174996+linehoon@users.noreply.github.com>
2025-03-27 17:21:44 +0900

    Update ask-free.js

--- pages/api/ask-free.js
+++ pages/api/ask-free.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { query } = req.body;
 
-  const response = await fetch('https://api-inference.huggingface.co/models/openchat/openchat-3.5-0106', {
+  const response = await fetch('https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-alpha', {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
